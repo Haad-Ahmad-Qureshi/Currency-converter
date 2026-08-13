127eafe6810216caffb6bc1433998e9856137132
Haad Ahmad Qureshi <haadahmadqureshi@gmail.com>
2026-08-12 21:39:47 -0700

    completed all function linked properly the api msg is displaying exchange rate

--- script.js
+++ script.js
@@ -44,3 +44,47 @@ const updateFlag = (element) => {
   img.setAttribute("src", newSrc);
 };
 
+// Convert currency
+btn.addEventListener("click", async (evt) => {
+  evt.preventDefault();
+
+  let amount = document.querySelector(".amount input");
+
+  // If amount is empty or negative
+  if (amount.value === "" || amount.value < 0) {
+    amount.value = "1";
+  }
+
+  let from = fromCurr.value.toLowerCase();
+  let to = toCurr.value.toLowerCase();
+
+  // Current API structure:
+  // /usd.json
+  // /eur.json
+  // /pkr.json
+
+  const URL = `${BASE_URL}/${from}.json`;
+
+  try {
+    let response = await fetch(URL);
+
+    if (!response.ok) {
+      throw new Error("Currency API request failed");
+    }
+
+    let data = await response.json();
+
+    // Example:
+    // data.usd.pkr
+    let rate = data[from][to];
+
+    let finalAmount = amount.value * rate;
+
+    msg.innerText = `${amount.value} ${fromCurr.value} = ${finalAmount.toFixed(
+      2
+    )} ${toCurr.value}`;
+  } catch (error) {
+    console.error(error);
+    msg.innerText = "Unable to get exchange rate. Please try again.";
+  }
+});
